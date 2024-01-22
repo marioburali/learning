aabf551393f9b22609985e50f181cc5705a7456d
Mario Burali <marioburali@hotmail.com>
2024-01-22 08:26:24 -0300

    js

--- teste.js
+++ teste.js
@@ -1,6 +1,6 @@
-
-var userName = 'Isabella';
-console.log(userName)
-
-var userName = 'Lucas';
-console.log(userName);
+const myName = 'Mario Burali'
+const city = 'Maringá'
+const birthYear = 1996
+console.log(myName)
+console.log(city)
+console.log(birthYear)
