1386639398e85ab4fe02a19a827386126957ba29
Mario Burali <marioburali@hotmail.com>
2024-01-22 08:47:19 -0300

    aula js

--- teste.js
+++ teste.js
@@ -4,3 +4,11 @@ const birthYear = 1996
 console.log(myName)
 console.log(city)
 console.log(birthYear)
+
+const base = 5
+const height = 8
+const area = base * height
+const perimeter = base + height
+
+console.log(area);
+console.log(perimeter);
